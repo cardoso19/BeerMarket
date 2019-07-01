4458dac8be2727e67043125d603451c23864edcb
Matheus Cardoso Kuhn <matheuscardosokuhn@hotmail.com>
2019-07-01 20:10:48 -0300

    Added base network support for requests and to download images.

---
+++ BeerMarket/Models/ErrorString.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorString.swift
+//  BeerMarket
+//
+//  Created by Matheus Cardoso kuhn on 01/07/19.
+//  Copyright © 2019 MDT. All rights reserved.
+//
+
+import Foundation
+
+struct ErrorString: Error {
+    let description: String
+}
---
+++ BeerMarket/Util/ConnectionUtil.swift
@@ -0,0 +1,53 @@
+//
+//  ConnectionUtil.swift
+//  BeerMarket
+//
+//  Created by Matheus Cardoso kuhn on 01/07/19.
+//  Copyright © 2019 MDT. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ConnectionUtil {
+    static let baseURL: String = "https://api.punkapi.com/v2/"
+    
+    static func request<T: Codable>(on endPoint: String, method: HTTPMethod, parameters: Parameters, completion: @escaping (_ result: Result<T>) -> Void) {
+        Alamofire.SessionManager.default.request(baseURL + endPoint,
+                                                 method: method,
+                                                 parameters: parameters)
+            .responseJSON { response in
+                if let error = response.error {
+                    completion(.failure(error))
+                } else {
+                    do {
+                        if let data = response.data {
+                            let responseData = try JSONDecoder().decode(T.self, from: data)
+                            completion(.success(responseData))
+                        } else {
+                            completion(.failure(ErrorString(description: "The response object is empty")))
+                        }
+                    } catch {
+                        completion(.failure(error))
+                    }
+                }
+        }
+    }
+    
+    static func downloadImage(on urlString: String, completion: @escaping (_ result: Result<UIImage>) -> Void) {
+        if let url = URL(string: urlString) {
+            let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error))
+                } else if let data = data, let image = UIImage(data: data) {
+                    completion(.success(image))
+                } else {
+                    completion(.failure(ErrorString(description: "Problem reading the response data")))
+                }
+            }
+            task.resume()
+        } else {
+            completion(.failure(ErrorString(description: "Invalid URL")))
+        }
+    }
+}
